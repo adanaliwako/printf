f714650f8a1803465e4ed6960929879f761fa8fc
adanaliwako <adanaliwako81@gmail.com>
2023-08-22 11:49:00 -0700

    commit

---
+++ write.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * The character to print
+ * On success 1.
+ * On error
+ * uses a local buffer of 1024
+ */
+int _putchar(char c)
+{
+	static char buf[1024];
+	static int i;
+
+	if (c == -1 || i >= 1024)
+	{
+		write(1, &buf, i);
+		i = 0;
+	}
+	if (c != -1)
+	{
+		buf[i] = c;
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * prints a string to stdout
+ * pointer to the string to print
+ * number of chars written
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	return (i);
+}
